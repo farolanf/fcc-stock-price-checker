131be22837243d13ca1adb6bcfc9adcb90c30ac5
Glitch (gusty-tern) <none>
2018-11-14 00:17:42 +0000

    ✈️🕷 Checkpoint ./models.js:1601241/4996 ./routes/api.js:1601241/1903

--- models.js
+++ models.js
@@ -1,4 +1,70 @@
+var axios = require('axios');
+var _ = require('lodash');
+
+const avKey = process.env.ALPHAVANTAGE_APIKEY
+
 function Stock (db) {
+  const stocks = db.collection('stocks');
+  const stockLikes = db.collection('stockLikes');
+
+  this.getStockData = getStockData;
+  
+  function getStockData (stock, like, ip) {
+    return new Promise((resolve, reject) => {
+      loadStock(stock)
+        .then(doc => {
+          if (doc) return process(doc)
+          return fetchStock(stock)
+            .then(saveStock)
+            .then(process)
+            .catch(reject)
+        })
+        .catch(reject)
+
+      async function process(data) {
+        await likeStock(like, data.stock, ip);
+        resolveDoc(data);
+      }
+      
+      function resolveDoc(data) {
+        resolve(_.pick(data, ['stock', 'price', 'likes']));
+      }
+    });
+  }
+
+  async function hasLike(stock, ip) {
+    return !! await db.collection('stockLikes').findOne({ stock, ip })
+  }
+  
+  function likeStock(stock, ip) {
+    db.collection('stockLikes').findOneAndUpdate(
+      { stock, ip }, 
+      { $set: { stock, ip } },
+      { upsert: true }
+    );
+  }
+  
+  function saveStock (data) {
+    return db.collection('stocks').findOneAndUpdate(
+      { stock: data.stock }, 
+      { $set: data }, 
+      { upsert: true, returnOriginal: false }
+    ).then(result => result.value)
+  }
+  
+  function loadStock (stock) {
+    return db.collection('stocks').findOne({ stock })
+  }
+
+  function fetchStock (stock) {
+    return axios.get(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stock}&apikey=${avKey}`)
+      .then(resp => ({
+        stock: resp.data['Global Quote']['01. symbol'],
+        price: resp.data['Global Quote']['05. price'],
+      }));
+  }
 }
 
-module.exports = Stock
+module.exports = {
+  Stock
+}
--- routes/api.js
+++ routes/api.js
@@ -9,70 +9,13 @@
 'use strict';
 
 var expect = require('chai').expect;
-var axios = require('axios');
-var _ = require('lodash');
-
-const avKey = process.env.ALPHAVANTAGE_APIKEY
+var { Stock } = require('../models');
 
 module.exports = function (app, db) {
 
   app.route('/api/stock-prices')
     .get(async function (req, res){
-      res.send({ stockData: await getStockData(req.query.stock, req.query.like, req.ip) });
+      const stock = new Stock(db);
+      res.send({ stockData: await stock.getStockData(req.query.stock, req.query.like, req.ip) });
     });
-  
-  function getStockData (stock, like, ip) {
-    return new Promise((resolve, reject) => {
-      loadStock(stock)
-        .then(doc => {
-          if (doc) return process(doc)
-          fetchStock(stock)
-            .then(process)
-            .catch(reject)
-        })
-        .catch(reject)
-
-      async function process(data) {
-        await likeStock(like, data.stock, ip);
-        return saveStock(data).then(resolveDoc)
-        resolveDoc(data);
-      }
-      
-      function resolveDoc(data) {
-        resolve(_.pick(data, ['stock', 'price', 'likes']));
-      }
-    });
-  }
-
-  async function hasLike(stock, ip) {
-    return !! await db.collection('stockLikes').findOne({ stock, ip })
-  }
-  
-  function likeStock(stock, ip) {
-    db.collection('stockLikes').findOneAndUpdate(
-      { stock, ip }, 
-      { $set: { stock, ip } },
-      { upsert: true }
-    );
-  }
-  
-  function saveStock (data) {
-    return db.collection('stocks').findOneAndUpdate(
-      { stock: data.stock }, 
-      { $set: data }, 
-      { upsert: true, returnOriginal: false }
-    ).then(result => result.value)
-  }
-  
-  function loadStock (stock) {
-    return db.collection('stocks').findOne({ stock })
-  }
-
-  function fetchStock (stock) {
-    return axios.get(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stock}&apikey=${avKey}`)
-      .then(resp => ({
-        stock: resp.data['Global Quote']['01. symbol'],
-        price: resp.data['Global Quote']['05. price'],
-      }));
-  }
 };
