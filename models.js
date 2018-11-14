105065dfc34d90c9d24eb461f818211e504affe1
Glitch (gusty-tern) <none>
2018-11-14 00:37:20 +0000

    🕴🐝 Checkpoint ./models.js:1601241/1775 ./routes/api.js:1601241/1714

--- models.js
+++ models.js
@@ -4,58 +4,35 @@ var _ = require('lodash');
 const avKey = process.env.ALPHAVANTAGE_APIKEY
 
 function Stock (db) {
-  const stocks = db.collection('stocks');
   const stockLikes = db.collection('stockLikes');
 
   this.getStockData = getStockData;
   
   function getStockData (stock, like, ip) {
-    return new Promise((resolve, reject) => {
-      loadStock(stock)
-        .then(doc => {
-          if (doc) return process(doc)
-          return fetchStock(stock)
-            .then(saveStock)
-            .then(process)
-            .catch(reject)
-        })
-        .catch(reject)
-
-      async function process(data) {
-        await likeStock(like, data.stock, ip);
-        resolveDoc(data);
-      }
-      
-      function resolveDoc(data) {
-        resolve(_.pick(data, ['stock', 'price', 'likes']));
-      }
-    });
+    return fetchStock(stock)
+      .then(async stockData => {
+        await likeStock(like, stock, ip);
+        stockData.likes = await getStockLikes(stock);
+        return stockData
+      })
   }
 
-  async function hasLike(stock, ip) {
-    return !! await db.collection('stockLikes').findOne({ stock, ip })
+  async function getStockLikes(stock) {
+    return await stockLikes.count({ stock })
   }
   
-  function likeStock(stock, ip) {
-    db.collection('stockLikes').findOneAndUpdate(
-      { stock, ip }, 
-      { $set: { stock, ip } },
-      { upsert: true }
-    );
+  async function likeStock(like, stock, ip) {
+    if (like) {
+      await stockLikes.findOneAndUpdate(
+        { stock, ip }, 
+        { $set: { stock, ip } },
+        { upsert: true }
+      );
+    } else {
+      await stockLikes.deleteOne({ stock, ip })
+    }
   }
   
-  function saveStock (data) {
-    return db.collection('stocks').findOneAndUpdate(
-      { stock: data.stock }, 
-      { $set: data }, 
-      { upsert: true, returnOriginal: false }
-    ).then(result => result.value)
-  }
-  
-  function loadStock (stock) {
-    return db.collection('stocks').findOne({ stock })
-  }
-
   function fetchStock (stock) {
     return axios.get(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stock}&apikey=${avKey}`)
       .then(resp => ({
--- routes/api.js
+++ routes/api.js
@@ -16,6 +16,15 @@ module.exports = function (app, db) {
   app.route('/api/stock-prices')
     .get(async function (req, res){
       const stock = new Stock(db);
-      res.send({ stockData: await stock.getStockData(req.query.stock, req.query.like, req.ip) });
+      if (Array.isArray(req.query.stock)) {
+        const stockData1 = await stock.getStockData(req.query.stock[0], req.query.like, req.ip);
+        const stockData2 = await stock.getStockData(req.query.stock[1], req.query.like, req.ip);
+        const likes1 = stockData1.likes;
+        stockData1.likes = likes1 - stockData2.likes;
+        stockData2.likes = stockData2.likes - likes1;
+        res.send({ stockData: [ stockData1, stockData2 ]});
+      } else {
+        res.send({ stockData: await stock.getStockData(req.query.stock, req.query.like, req.ip) });
+      }
     });
 };
